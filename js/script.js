2f73041ed75b88775a9c533f892f7c85e2328026
jiahoe <mrjiahoe@hotmail.com>
2022-09-27 19:02:53 +0800

    generate qrcode

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const qr = document.getElementById("qrcode");
 const onGenerateSubmit = (e) => {
 	e.preventDefault();
 
+	clearUI();
+
 	const url = document.getElementById("url").value;
 	const size = document.getElementById("size").value;
 
@@ -14,8 +16,23 @@ const onGenerateSubmit = (e) => {
 
 		setTimeout(() => {
 			hideSpinner();
+
+			generateQRCode(url, size);
+
+			setTimeout(() => {
+				const saveUrl = qr.querySelector("img").src;
+				createSaveBtn(saveUrl);
+			}, 50);
 		}, 1000);
 	}
+};
+
+const generateQRCode = (url, size) => {
+	const qrcode = new QRCode("qrcode", {
+		text: url,
+		width: size,
+		height: size,
+	});
 };
 
 const showSpinner = () => {
@@ -24,6 +41,23 @@ const showSpinner = () => {
 
 const hideSpinner = () => {
 	document.getElementById("spinner").style.display = "none";
+};
+
+const clearUI = () => {
+	qr.innerHTML = "";
+	const saveLink = document.getElementById("save-link");
+	if (saveLink) saveLink.remove();
+};
+
+const createSaveBtn = (saveUrl) => {
+	const link = document.createElement("a");
+	link.id = "save-link";
+	link.classList =
+		"bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+	link.href = saveUrl;
+	link.download = "qrcode";
+	link.innerHTML = "Save Image";
+	document.getElementById("generated").appendChild(link);
 };
 
 hideSpinner();
